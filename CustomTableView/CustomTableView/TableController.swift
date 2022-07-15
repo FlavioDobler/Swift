61d5d48caabc24b2f65a28ce048209db26830a6a
Flavio Dobler <flaviodobler@Flavios-MacBook-Air.local>
2022-07-15 01:59:32 -0300

    TableView With Data

--- CustomTableView/CustomTableView/TableController.swift
+++ CustomTableView/CustomTableView/TableController.swift
@@ -11,10 +11,13 @@ import Foundation
 class TableController: UIViewController {
 
     let tableView = UITableView()
-    let arrayTable = ["Dude", "What", "The", "Fuck", "Is", "A", "TableView"]
+    var data = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for x in 1...100 {
+            data.append("Some Stuff \(x)")
+        }
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
@@ -38,15 +41,19 @@ extension TableController : UITableViewDelegate {
 
 extension TableController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayTable.count
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = arrayTable[indexPath.row]
+        cell.textLabel?.text = data[indexPath.row]
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("Cell Tapped")
+    }
     
     
 }
